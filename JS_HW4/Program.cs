7d0e95240c61cdddd4cb7834babdcceb497341cb
Arman KZ <armansauyenov@gmail.com>
2018-12-02 11:22:23 +0600

    Добавьте файлы проекта.

---
+++ JS_HW4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JS_HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
